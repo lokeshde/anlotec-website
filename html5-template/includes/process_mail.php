0ba31046b8d4e4fa97435b7bf4bded150b6ea175
Yashas Lokesh <yashas.lokesh@gmail.com>
2018-07-25 20:02:56 -0400

    Fixed mispelling in process_mail.php

--- html5-template/includes/process_mail.php
+++ html5-template/includes/process_mail.php
@@ -32,7 +32,7 @@ if (!$suspect) :
     // and reassign expected elements to simple variables
 	foreach( $_POST as $key => $value) {
 		$value = is_array($value) ? $value : trim($value);
-		if(empty($value) && in_array($key, $reqired)) {
+		if(empty($value) && in_array($key, $required)) {
 			$missing[] = $key;
 			$$key = '';
 		}elseif (in_array($key, $expected)) {
